8914b0be53649e318c9be86edd4392289181cefb
ipatate <faramazpat@gmail.com>
2023-01-03 10:30:52 +0100

    update vite to version 4, fix token asset function

--- inc/core/assets.php
+++ inc/core/assets.php
@@ -5,10 +5,17 @@ namespace PressWind\inc\core\assets;
 function getTokenName($key)
 {
   // get token file
-  $k = explode('.', $key);
+  // model $key assets/main-legacy-fe2da1bc.js
+  $k = explode('-', $key);
   $token = $key;
+  // ex: $k[1] | $k[2] = fe2da1bc.js
   if (array_key_exists(1, $k)) {
-    $token = $k[1];
+    // take key 1 or 2
+    $t = array_key_exists(2, $k) ? explode('.', $k[2]) : explode('.', $k[1]);
+    // ex: $kt[0] = fe2da1bc
+    if (array_key_exists(0, $t)) {
+      $token = $t[0];
+    }
   }
   return $token;
 }
